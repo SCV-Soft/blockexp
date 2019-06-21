5185e542f5bb6e2163c7d638b717315cc0f57bfc
EcmaXp <ecmaxp@ecmaxp.pe.kr>
2019-06-21 19:47:38 +0900

    feat(swagger): mount swagger at /

--- blockexp/application.py
+++ blockexp/application.py
@@ -1,10 +1,12 @@
 from starlette.applications import Starlette
 
-from .api import api
+from swagger.ui import create_swagger
+from .api import api, apispec
 
 
 def init_app(*, debug=False) -> Starlette:
     app = Starlette(debug=debug)
     app.mount('/api', api)
+    app.mount('/', create_swagger(apispec))
 
     return app
