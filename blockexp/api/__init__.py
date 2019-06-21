3abfaa1d43fa85b8070768e194744093a6f72cdb
EcmaXp <ecmaxp@ecmaxp.pe.kr>
2019-06-21 19:43:02 +0900

    feat(api): add apispec, api

---
+++ blockexp/api/__init__.py
@@ -0,0 +1,20 @@
+from apispec import APISpec
+from apispec.ext.marshmallow import MarshmallowPlugin
+from starlette.routing import Router
+
+from starlette_typed import TypedStarlettePlugin
+from starlette_typed.apispec import patch_apispec_orderdict
+
+patch_apispec_orderdict()
+
+apispec = APISpec(
+    title=__package__,
+    version="1.0.0",
+    openapi_version="3.0.2",
+    plugins=[
+        TypedStarlettePlugin(),
+        MarshmallowPlugin(),
+    ],
+)
+
+api = Router()
