7c4b2d144825fcf092ea1b490f909679b29707b4
EcmaXp <ecmaxp@ecmaxp.pe.kr>
2019-06-21 19:47:16 +0900

    feat(api): mount api at /api

--- blockexp/application.py
+++ blockexp/application.py
@@ -1,7 +1,10 @@
 from starlette.applications import Starlette
 
+from .api import api
+
 
 def init_app(*, debug=False) -> Starlette:
     app = Starlette(debug=debug)
+    app.mount('/api', api)
 
     return app
