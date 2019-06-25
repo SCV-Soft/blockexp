9a0d90f22289e225b41d65e849622e395699400e
EcmaXp <ecmaxp@ecmaxp.pe.kr>
2019-06-25 18:07:25 +0900

    feat(provider): add context based provider argument

---
+++ blockexp/provider/__init__.py
@@ -0,0 +1,29 @@
+from contextlib import asynccontextmanager
+
+from starlette.requests import Request
+
+from starlette_typed.endpoint import register_handler
+from .base import Provider
+from .bitcore import BitcoreProvider
+
+
+@register_handler
+@asynccontextmanager
+async def provider(request: Request) -> Provider:
+    path_params = request.path_params
+    chain = path_params['chain']
+    network = path_params['network']
+
+    # noinspection PyShadowingNames
+    provider = get_provider(chain, network)
+
+    if "async with":
+        # TODO: async with?
+        yield provider
+
+
+def get_provider(chain: str, network: str) -> Provider:
+    if chain == "BTC" or chain == "BCH":
+        return BitcoreProvider(chain, network)
+    else:
+        raise NotImplementedError((chain, network))
