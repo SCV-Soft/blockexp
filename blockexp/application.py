c18fdf8d221c23f4ca0097f44919ac73bbcb1f69
EcmaXp <ecmaxp@ecmaxp.pe.kr>
2019-06-25 18:08:34 +0900

    feat(application): replace Starlette with Application

--- blockexp/application.py
+++ blockexp/application.py
@@ -1,11 +1,26 @@
+from typing import Any
+
 from starlette.applications import Starlette
 
 from swagger.ui import create_swagger
 from .api import api, apispec
 
 
+class Application(Starlette):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.extensions = {}
+        self.config = {}
+
+    def get_extension(self, name: str):
+        return self.extensions[name]
+
+    def register_extension(self, extension: Any):
+        self.extensions[extension.__name__] = extension.init_app(self)
+
+
 def init_app(*, debug=False) -> Starlette:
-    app = Starlette(debug=debug)
+    app = Application(debug=debug)
     app.mount('/api', api)
     app.mount('/', create_swagger(apispec))
 
