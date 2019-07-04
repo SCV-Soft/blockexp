40748103a35e01949a3079d0efea750ef0415166
EcmaXp <ecmaxp@ecmaxp.pe.kr>
2019-07-04 21:30:36 +0900

    feat(error): add NotFound, ObjectNotFound, BlockNotFound, TransactionNotFound, AddressNotFound

--- blockexp/error/__init__.py
+++ blockexp/error/__init__.py
@@ -8,3 +8,23 @@ class HTTPError(HTTPException):
 
     def __init__(self, detail: str = None):
         super().__init__(self.status_code, detail)
+
+
+class NotFound(HTTPError):
+    status_code = HTTPStatus.NOT_FOUND
+
+
+class ObjectNotFound(NotFound, KeyError):
+    pass
+
+
+class BlockNotFound(ObjectNotFound):
+    pass
+
+
+class TransactionNotFound(ObjectNotFound):
+    pass
+
+
+class AddressNotFound(ObjectNotFound):
+    pass
