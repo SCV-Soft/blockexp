930a3420cd0ba0ec783e2516c2e4af4534a9cf4e
EcmaXp <ecmaxp@ecmaxp.pe.kr>
2019-07-04 17:28:01 +0900

    feat(api): change get_daily_transaction signature

--- blockexp/api/bitcore/stats.py
+++ blockexp/api/bitcore/stats.py
@@ -1,16 +1,15 @@
-from typing import Any
-
 from starlette.requests import Request
 from starlette.routing import Router
 
 from starlette_typed import typed_endpoint
 from . import ApiPath
 from ...provider import Provider
+from ...provider.base import DailyTransactions
 
 api = Router()
 
 
 @api.route('/daily-transactions', methods=['GET'])
 @typed_endpoint(tags=["bitcore"])
-async def get_daily_transactions(request: Request, path: ApiPath, provider: Provider) -> Any:
+async def get_daily_transactions(request: Request, path: ApiPath, provider: Provider) -> DailyTransactions:
     return await provider.get_daily_transactions()
