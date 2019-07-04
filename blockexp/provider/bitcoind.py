66b7677787416f0e8f8bc413a620bd3c86168316
EcmaXp <ecmaxp@ecmaxp.pe.kr>
2019-07-04 17:23:14 +0900

    feat(provider): add JackDaemonProvider

--- blockexp/provider/bitcoind.py
+++ blockexp/provider/bitcoind.py
@@ -258,6 +258,26 @@ class BitcoinDaemonProvider(Provider):
         return await self.get_block(block_hash)
 
 
+class JackDaemonProvider(BitcoinDaemonProvider):
+    def _convert_raw_transaction(self, transaction: dict) -> BtcTransaction:
+        transaction['hash'] = transaction['txid']
+        transaction['vsize'] = transaction['size']
+        transaction['weight'] = -1
+        return BtcTransaction(**transaction)
+
+    def _convert_raw_block(self, block: dict) -> BtcBlock:
+        del block['acc_checkpoint']
+        del block['modifier']
+        del block['moneysupply']
+        del block['zJACKsupply']
+        block['strippedsize'] = block['size']
+        block['weight'] = -1
+        block['versionHex'] = '-'
+        block['nTx'] = len(block['tx'])
+
+        return BtcBlock(**block)
+
+
 class BitcoinMongoProvider(Provider):
     def __init__(self, chain: str, network: str, database: MongoDatabase, provider: BitcoinDaemonProvider = None):
         super().__init__(chain, network)
