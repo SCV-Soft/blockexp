c2e259de69ea92c53bfef93483e709f7b9b10afb
EcmaXp <ecmaxp@ecmaxp.pe.kr>
2019-07-18 20:04:19 +0900

    fix(starlette_typed): add cache_endpoint in module

--- starlette_typed/__init__.py
+++ starlette_typed/__init__.py
@@ -2,10 +2,11 @@
 
 __all__ = [
     "typed_endpoint",
+    "cache_endpoint",
     "TypedStarlettePlugin",
     "TypedStarletteSchemaGenerator",
 ]
 
 from .apispec import TypedStarlettePlugin
-from .endpoint import typed_endpoint
+from .endpoint import typed_endpoint, cache_endpoint
 from .starlette import TypedStarletteSchemaGenerator
