b3f501b2a88f78d73fa398d6bd3c0ef373818a4e
EcmaXp <ecmaxp@ecmaxp.pe.kr>
2019-07-25 15:35:20 +0900

    fix(types): fix types package

--- blockexp/types/__init__.py
+++ blockexp/types/__init__.py
@@ -1,6 +1,7 @@
 __all__ = ["Blockchain", "Importer", "Provider", "RawProvider", "ProviderType", "Service"]
 
+from .accessor import Accessor
 from .blockchain import Blockchain
 from .importer import Importer
-from .provider import Provider, RawProvider, ProviderType
+from .provider import Provider
 from .service import Service
