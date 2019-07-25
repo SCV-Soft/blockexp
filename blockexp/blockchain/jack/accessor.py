e093bda8f8a35388ea6b8b18743c481755cc7817
EcmaXp <ecmaxp@ecmaxp.pe.kr>
2019-07-25 16:29:15 +0900

    feat(jack): apply legacy types

--- blockexp/blockchain/jack/accessor.py
+++ blockexp/blockchain/jack/accessor.py
@@ -4,9 +4,7 @@ from ..btc.accessor import BitcoinDaemonAccessor
 
 class JackDaemonAccessor(BitcoinDaemonAccessor):
     def _convert_raw_transaction(self, transaction: dict) -> JackTransaction:
-        JackTransaction.fix_tx(transaction)
-        return JackTransaction(**transaction)
+        return JackTransaction.load(transaction)
 
     def _convert_raw_block(self, block: dict) -> JackBlock:
-        JackBlock.fix_block(block)
-        return JackBlock(**block)
+        return JackBlock.load(block)
--- blockexp/blockchain/jack/types.py
+++ blockexp/blockchain/jack/types.py
@@ -1,33 +1,14 @@
 from dataclasses import dataclass
 from typing import Any
 
-from ..btc.types import BtcBlock, BtcTransaction
+from ..btc.legacy import LegacyBtcTransaction, LegacyBtcBlock
+
+
+class JackTransaction(LegacyBtcTransaction):
+    pass
 
 
 @dataclass
-class JackBlock(BtcBlock):
-    acc_checkpoint: Any = None
-    modifier: Any = None
-    moneysupply: Any = None
+class JackBlock(LegacyBtcBlock):
+    TRANSACTION_CLASS = JackTransaction  # used in parse_tx
     zJACKsupply: Any = None
-
-    @classmethod
-    def fix_block(cls, raw_block):
-        raw_block.setdefault('strippedsize', raw_block['size'])
-        raw_block.setdefault('weight', -1)
-        raw_block.setdefault('versionHex', '')
-        raw_block.setdefault('nTx', len(raw_block['tx']))
-
-    @classmethod
-    def parse_tx(cls, raw_tx):
-        JackTransaction.fix_tx(raw_tx)
-        return JackTransaction(**raw_tx)
-
-
-@dataclass
-class JackTransaction(BtcTransaction):
-    @classmethod
-    def fix_tx(cls, raw_tx):
-        raw_tx.setdefault('hash', raw_tx['txid'])
-        raw_tx.setdefault('vsize', raw_tx['size'])
-        raw_tx.setdefault('weight', -1)
