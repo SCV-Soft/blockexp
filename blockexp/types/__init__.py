a6d76383e0f0eac1e0808aba7349e1c95f37b062
EcmaXp <ecmaxp@ecmaxp.pe.kr>
2019-07-25 15:35:00 +0900

    refactor(service): move Service to types

--- blockexp/types/__init__.py
+++ blockexp/types/__init__.py
@@ -1,5 +1,6 @@
-__all__ = ["Blockchain", "Importer", "Provider", "RawProvider", "ProviderType"]
+__all__ = ["Blockchain", "Importer", "Provider", "RawProvider", "ProviderType", "Service"]
 
 from .blockchain import Blockchain
 from .importer import Importer
 from .provider import Provider, RawProvider, ProviderType
+from .service import Service
