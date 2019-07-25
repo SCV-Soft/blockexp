4ef4f224b74c121c1c858e5c3113553639dbfe8b
EcmaXp <ecmaxp@ecmaxp.pe.kr>
2019-07-25 15:39:52 +0900

    refactor(utils): move parse_url

---
+++ blockexp/utils/url.py
@@ -0,0 +1,16 @@
+from typing import Tuple, Optional
+from urllib.parse import ParseResult, urlparse
+
+from requests.auth import HTTPBasicAuth
+
+
+def parse_url(url: str) -> Tuple[str, Optional[HTTPBasicAuth]]:
+    pr: ParseResult = urlparse(url)
+    if pr.username or pr.password:
+        assert pr.username and pr.password
+        auth = HTTPBasicAuth(pr.username, pr.password)
+        url = url.replace(f'{pr.username}:{pr.password}@', '')
+    else:
+        auth = None
+
+    return url, auth
