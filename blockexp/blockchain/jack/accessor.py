5aabec1093efa1ba73424572d5a54e1b75537356
EcmaXp <ecmaxp@ecmaxp.pe.kr>
2019-07-25 16:00:00 +0900

    feat(jack): add JackBlock, JackTransaction

--- blockexp/blockchain/jack/accessor.py
+++ blockexp/blockchain/jack/accessor.py
@@ -1,22 +1,12 @@
+from .types import JackBlock, JackTransaction
 from ..btc.accessor import BitcoinDaemonAccessor
-from ..btc.types import BtcTransaction, BtcBlock
 
 
 class JackDaemonAccessor(BitcoinDaemonAccessor):
-    def _convert_raw_transaction(self, transaction: dict) -> BtcTransaction:
-        transaction['hash'] = transaction['txid']
-        transaction['vsize'] = transaction['size']
-        transaction['weight'] = -1
-        return BtcTransaction(**transaction)
+    def _convert_raw_transaction(self, transaction: dict) -> JackTransaction:
+        JackTransaction.fix_tx(transaction)
+        return JackTransaction(**transaction)
 
-    def _convert_raw_block(self, block: dict) -> BtcBlock:
-        del block['acc_checkpoint']
-        del block['modifier']
-        del block['moneysupply']
-        del block['zJACKsupply']
-        block['strippedsize'] = block['size']
-        block['weight'] = -1
-        block['versionHex'] = '-'
-        block['nTx'] = len(block['tx'])
-
-        return BtcBlock(**block)
+    def _convert_raw_block(self, block: dict) -> JackBlock:
+        JackBlock.fix_block(block)
+        return JackBlock(**block)
