b350ed1d2a469fe52ac3d8310f5440ee11d93984
EcmaXp <ecmaxp@ecmaxp.pe.kr>
2019-07-25 16:28:53 +0900

    feat(pch): apply legacy types

--- blockexp/blockchain/pch/accessor.py
+++ blockexp/blockchain/pch/accessor.py
@@ -4,9 +4,7 @@ from ..btc.accessor import BitcoinDaemonAccessor
 
 class PchDaemonAccessor(BitcoinDaemonAccessor):
     def _convert_raw_transaction(self, transaction: dict) -> PchTransaction:
-        PchTransaction.fix_tx(transaction)
-        return PchTransaction(**transaction)
+        return PchTransaction.load(transaction)
 
     def _convert_raw_block(self, block: dict) -> PchBlock:
-        PchBlock.fix_block(block)
-        return PchBlock(**block)
+        return PchBlock.load(block)
