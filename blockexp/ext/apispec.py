a4d4c43018f2b1d285720409b3d028e83ed5d045
EcmaXp <ecmaxp@ecmaxp.pe.kr>
2019-08-12 17:28:15 +0900

    fix(apispec): lazy init apispec for apply patch_apispec_orderdict

--- blockexp/ext/apispec.py
+++ blockexp/ext/apispec.py
@@ -5,16 +5,19 @@ from starlette_typed import TypedStarlettePlugin
 from starlette_typed.apispec import patch_apispec_orderdict
 from ..application import Application
 
-apispec = APISpec(
-    title=__package__,
-    version="1.0.0",
-    openapi_version="3.0.2",
-    plugins=[
-        TypedStarlettePlugin(),
-        MarshmallowPlugin(),
-    ],
-)
+apispec: APISpec
 
 
 async def init_app(app: Application):
+    global apispec
     patch_apispec_orderdict()
+
+    apispec = APISpec(
+        title=__package__,
+        version="1.0.0",
+        openapi_version="3.0.2",
+        plugins=[
+            TypedStarlettePlugin(),
+            MarshmallowPlugin(),
+        ],
+    )
