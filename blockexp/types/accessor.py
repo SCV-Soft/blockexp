26f90ff9a4f2f64b988a521264be1588fb704b9d
EcmaXp <ecmaxp@ecmaxp.pe.kr>
2019-09-09 15:11:19 +0900

    feat(types): Accessor inherit Connectable, with ABC

--- blockexp/types/accessor.py
+++ blockexp/types/accessor.py
@@ -1,28 +1,28 @@
+from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from typing import Union, TypeVar
 
 from ._base import Base
+from .connectable import Connectable
 from ..model import Block, Transaction, EstimateFee
 
 T = TypeVar('T')
 
 
 @dataclass(init=False)
-class Accessor(Base):
-    async def __aenter__(self):
-        return self
-
-    async def __aexit__(self, exc_type, exc_val, exc_tb):
-        pass
-
+class Accessor(Connectable, Base, ABC):
+    @abstractmethod
     async def get_block(self, block_id: Union[str, int]) -> Block:
         raise NotImplementedError
 
+    @abstractmethod
     async def get_transaction(self, tx_id: str) -> Transaction:
         raise NotImplementedError
 
+    @abstractmethod
     async def get_fee(self, target: int) -> EstimateFee:
         raise NotImplementedError
 
+    @abstractmethod
     async def get_local_tip(self) -> Block:
         raise NotImplementedError
